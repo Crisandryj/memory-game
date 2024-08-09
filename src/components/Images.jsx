6d148a113b683404e6a28e3bf530464f54932c63
Crisandry Javier <crisandryj@gmail.com>
2024-08-09 16:07:47 -0400

    Feat: Fetch pokemon name and image and display

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,13 +1,34 @@
-function Images() {
-  function getPokemon(pokeName) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`).then((reponse) =>
-      reponse
-        .json()
-        .then((data) => console.log(data))
-        .catch((err) => {
-          console.log("Pokemon not found", err);
-        })
-    );
-  }
-  getPokemon("ditto");
+import { useState, useEffect } from "react";
+
+function Image() {
+  const [pokemon, setPokemon] = useState({ img: "", name: "" });
+  const urlPokeName = "ditto";
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${urlPokeName}`).then((reponse) =>
+    reponse
+      .json()
+      .then(
+        (data) => (
+          (setPokemon({
+            img: data.sprites.other["official-artwork"].front_default,
+            name: data.name,
+          }),
+          (pokemonName = data.name)),
+          console.log(pokeImg),
+          console.log(pokemonName)
+        )
+      )
+      .catch((err) => {
+        console.log("Pokemon not found", err);
+      })
+  );
+
+  return (
+    <>
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.img} alt="" />
+    </>
+  );
 }
+
+export default Image;
