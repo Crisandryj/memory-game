82df45c36708168d2cfab9640aab66c7731aac9c
Crisandry Javier <crisandryj@gmail.com>
2024-08-10 14:24:20 -0400

    Feat: create array of pokemon and map

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,27 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Image from "./components/Images";
+const pokemonArray = [
+  "Venusaur",
+  "Charmeleon",
+  "Blastoise",
+  "Charizard",
+  "Beedrill",
+  "Arbok",
+  "Raichu",
+  "Fearow",
+  "Clefairy",
+  "Primeape",
+];
 
 function App() {
   return (
     <>
       <div className="images">
-        <Image name="ditto" />
+        {pokemonArray.map((pokemon) => {
+          <Image name={pokemon} />;
+          console.log(pokemon);
+        })}
       </div>
     </>
   );
