625f22499044c7c000e5291dbf8d0538ec6c274b
Crisandry Javier <crisandryj@gmail.com>
2024-08-12 10:00:40 -0400

    Feat: Add id to pokemon and set as key

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,27 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Image from "./components/Images";
+import { v4 as uuidv4 } from "uuid";
+
 const pokemonArray = [
-  "Venusaur",
-  "Charmeleon",
-  "Blastoise",
-  "Charizard",
-  "Beedrill",
-  "Arbok",
-  "Raichu",
-  "Fearow",
-  "Clefairy",
-  "Primeape",
+  { name: "venusaur", id: uuidv4() },
+  {name:"charmeleon", id: uuidv4() }
+  {name:"blastoise", id: uuidv4() },
+  {name:"charizard", id: uuidv4() },
+  {name:"beedrill", id: uuidv4() },
+  {name:"Arbok", id: uuidv4() },
+  {name:"Raichu", id: uuidv4() },
+  {name:"Fearow", id: uuidv4() },
+  {name:"clefairy", id: uuidv4() },
+  {name:"primeape", id: uuidv4() }
 ];
-//map not showing pokemon images
+
 function App() {
   return (
     <>
       <div className="images">
         {pokemonArray.map((pokemon) => {
-          return <Image name={pokemon.toLowerCase()} />;
+          return <Image name={pokemon.name} key={pokemon.id}/>;
         })}
       </div>
     </>
