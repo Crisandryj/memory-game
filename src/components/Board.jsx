efebd5576bf8eba7fa753092e2996c7ec35e5c87
Crisandry Javier <crisandryj@gmail.com>
2024-08-13 13:25:57 -0400

    Feat: Move shuffle array to Board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,17 +1,31 @@
 import Image from "./Images";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function Board(props) {
-  const [array, setArray] = useState(props.pokemon);
-  useEffect(() => {
-    setArray(props.pokemon);
-  }, [props.pokemon]);
+  const [arry, setArry] = useState([
+    { name: "venusaur", id: uuidv4() },
+    { name: "charmeleon", id: uuidv4() },
+  ]);
+
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+  const onClick = () => {
+    setArry(shuffleArray(props.pokemon));
+  };
 
   return (
     <div className="images">
-      {props.pokemon.map((pokemon) => {
+      {arry.map((pokemon) => {
         return (
-          <button onClick={props.onClick} key={pokemon.id}>
+          <button onClick={onClick} key={pokemon.id}>
             <Image name={pokemon.name} key={pokemon.id} />
           </button>
         );
