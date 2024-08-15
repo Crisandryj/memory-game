36c51f5f8a1d27a69cda9753e5b62fdc5db17c95
Crisandry Javier <crisandryj@gmail.com>
2024-08-15 14:37:42 -0400

    Add scoreboard container

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 function Score(props) {
   return (
-    <>
+    <div className="scoreboard">
+      <h1>SCOREBOARD</h1>
       <h2>Count: {props.count}</h2>
       <h2>Best Score: {props.score}</h2>
-    </>
+    </div>
   );
 }
 
