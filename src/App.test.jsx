2c2c615e8c5f47f7da2988a51034cbcec7acda21
Crisandry Javier <crisandryj@gmail.com>
2024-09-06 15:33:26 -0400

    Comment

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,7 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "./App2";
 
+//test for testing
 describe("App2 component", () => {
   it("renders correct heading", () => {
     render(<App />);
